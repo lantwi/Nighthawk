0a0584ddd5aadc439a69d4bd0aa046fbae291397
Noah Springwater <nspringwater@gmail.com>
2015-11-23 14:17:07 -0500

    changed name to yelp.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ var express = require('express'),
    http = require('http').Server(app);
 
 var yelp = new Yelp({
- consumer_key: process.env.consumer_key,
- consumer_secret: process.env.consumer_secret,
- token: process.env.token,
- token_secret: process.env.token_secret
+ consumer_key: '8cszeFT7_E4zMENoUzmkuQ',
+ consumer_secret: 'zleZtp_5LRJl4O90yKsD4BAIhp4',
+ token: 'nnX71q_vcXn3jU8X0O1lMWNFBosUf0TD',
+ token_secret: 'BC31sF5RDHI54eORY7PBf0-wVS4',
 });
 
-app.use(logger('dev'));
+  app.use(logger('dev'));
 
-app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   yelp.search({ term: 'food', location: 'Manhattan' })
@@ -30,8 +30,207 @@ app.get('/', function(req, res){
   });
 });
 
+
+app.get('/financial_district', function(req, res){
+  yelp.search({ term: 'bars', location: 'financial district', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/chinatown', function(req, res){
+  yelp.search({ term: 'bars', location: 'chinatown', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/soho', function(req, res){
+  yelp.search({ term: 'bars', location: 'soho', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/lower_east_side', function(req, res){
+  yelp.search({ term: 'bars', location: 'lower east side', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/greenwich_village ', function(req, res){
+  yelp.search({ term: 'bars', location: 'Greenwich Village', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+app.get('/east_village', function(req, res){
+  yelp.search({ term: 'bars', location: 'east village', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/tribeca ', function(req, res){
+  yelp.search({ term: 'bars', location: 'Tribeca', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/gramercy_park', function(req, res){
+  yelp.search({ term: 'bars', location: 'Gramercy Park', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/chelsea', function(req, res){
+  yelp.search({ term: 'bars', location: 'Chelsea', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/midtown_west ', function(req, res){
+  yelp.search({ term: 'bars', location: 'Midtown West', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/midtown_east', function(req, res){
+  yelp.search({ term: 'bars', location: 'Midtown East', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/central_park', function(req, res){
+  yelp.search({ term: 'bars', location: 'Central Park', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/east_village', function(req, res){
+  yelp.search({ term: 'bars', location: 'east village', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/upper_west_side', function(req, res){
+  yelp.search({ term: 'bars', location: 'Upper West Side', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+
+app.get('/upper_east_side', function(req, res){
+  yelp.search({ term: 'bars', location: 'Upper East Side', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/harlem', function(req, res){
+  yelp.search({ term: 'bars', location: 'Harlem', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/inwood', function(req, res){
+  yelp.search({ term: 'bars', location: 'Inwood', limit: 10, sort: 2  })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
+app.get('/washington_heights', function(req, res){
+  yelp.search({ term: 'bars', location: 'washington heights', limit: 10, sort: 2 })
+    .then(function (data) {
+    console.log(data);
+    res.send(data)
+  })
+  .catch(function (err) {
+    console.error(err);
+  });
+});
+
    //Set up the port to listen
-app.listen(3000, function () {
+ app.listen(3000, function () {
  console.log('App listening on port 3000...');
    });
 
@@ -39,26 +238,26 @@ app.listen(3000, function () {
 
 
 
-// See http://www.yelp.com/developers/documentation/v2/search_api
-yelp.search({ term: 'food', location: 'Montreal' })
-.then(function (data) {
- console.log(data);
-})
-.catch(function (err) {
- console.error(err);
-});
-
-// See http://www.yelp.com/developers/documentation/v2/business
-yelp.business('yelp-san-francisco')
- .then(console.log)
- .catch(console.error);
-
-yelp.phoneSearch({ phone: '+15555555555' })
- .then(console.log)
- .catch(console.error);
-
-// A callback based API is also available:
-yelp.business('yelp-san-francisco', function(err, data) {
- if (err) return console.log(error);
- console.log(data);
-});
+// // See http://www.yelp.com/developers/documentation/v2/search_api
+// yelp.search({ term: 'food', location: 'Montreal' })
+// .then(function (data) {
+//  console.log(data);
+// })
+// .catch(function (err) {
+//  console.error(err);
+// });
+//
+// // See http://www.yelp.com/developers/documentation/v2/business
+// yelp.business('yelp-san-francisco')
+//  .then(console.log)
+//  .catch(console.error);
+//
+// yelp.phoneSearch({ phone: '+15555555555' })
+//  .then(console.log)
+//  .catch(console.error);
+//
+// // A callback based API is also available:
+// yelp.business('yelp-san-francisco', function(err, data) {
+//  if (err) return console.log(error);
+//  console.log(data);
+// });
